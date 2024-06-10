376faf2e61d2f58d9f47d90d8a7fef04e31c82be
ramalMr <ramalmirzeyev04@gmail.com>
2024-06-11 03:31:20 +0400

    evaluate_current_state funksiasinda deysiklik

--- src/analysis/state_evaluation.py
+++ src/analysis/state_evaluation.py
@@ -9,14 +9,11 @@ def find_empirical_values(indicator: pd.Series, quantile: float) -> Tuple[float,
     peak_threshold = indicator.quantile(quantile)
     return bottom_threshold, peak_threshold
 
-def evaluate_current_state(correlations: list, current_values: list, thresholds: list) -> float:
-    """
-    Korrelyasiyalar cari gosterici deyerleri ve hedlere gore cari veziyyeti qiymetlendirir
-    """
-    state = 0.0
+def evaluate_current_state(correlations, current_values, thresholds):
+    state = 0
     for correlation, current_value, threshold in zip(correlations, current_values, thresholds):
         if current_value < threshold[0]:
-            state += correlation * -1
-        elif current_value > threshold[1]:
             state += correlation
+        elif current_value > threshold[1]:
+            state -= correlation
     return state
